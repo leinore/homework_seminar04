fd2539bdd3e1a946b38091175611e4d5e26b29b9
leinore <116540768+leinore@users.noreply.github.com>
2022-11-26 20:54:42 +0300

    add task_27

---
+++ task_27/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+
+Console.Write("Введите число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while (A > 0)
+{
+    int B = A%10;
+    A = A/10;
+    sum = sum + B;
+}
+Console.WriteLine("Сумма цифр "+ sum);
